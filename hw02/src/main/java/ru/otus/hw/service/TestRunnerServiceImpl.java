8d24c0a18ab91b12fb7a41c1f781406751eef886
anetr <a.netrusov@mail.ru>
2025-04-27 17:35:14 +0300

    fix: handle exception

--- hw02/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
 
 @RequiredArgsConstructor
 @Service
@@ -13,10 +14,19 @@ public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final ResultService resultService;
 
+    private final IOService ioService;
+
     @Override
     public void run() {
-        var student = studentService.determineCurrentStudent();
-        var testResult = testService.executeTestFor(student);
-        resultService.showResult(testResult);
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (QuestionReadException exception) {
+            exception.printStackTrace();
+            ioService.printLine("Ошибка чтения вопросов!");
+        } catch (Throwable e) {
+            ioService.printLine("Неизвестная ошибка при выполнении!");
+        }
     }
 }
