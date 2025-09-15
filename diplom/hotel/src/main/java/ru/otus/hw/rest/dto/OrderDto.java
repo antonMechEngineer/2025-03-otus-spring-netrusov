d7cdcf4a21a021ef9d1f37b35958611f349507cb
anetr <a.netrusov@mail.ru>
2025-09-15 13:48:03 +0300

    fix: changed bean validation

--- diplom/hotel/src/main/java/ru/otus/hw/rest/dto/OrderDto.java
+++ diplom/hotel/src/main/java/ru/otus/hw/rest/dto/OrderDto.java
@@ -19,7 +19,6 @@ public class OrderDto {
     @Future(message = "End rent date must be in the future")
     private LocalDate endRent;
 
-    @NotNull(message = "Username cannot be null")
     @Size(max = 50, message = "Username cannot exceed 50 characters")
     private String username;
 
@@ -27,7 +26,7 @@ public class OrderDto {
     @Positive(message = "Room number must be positive")
     private Integer roomNumber;
 
-    @NotNull(message = "Total price cannot be null")
+
     @Positive(message = "Total price must be positive")
     @Digits(integer = 17, fraction = 2, message = "Total price must have maximum 17 integer digits and 2 fractional digits")
     private BigDecimal totalPrice;
