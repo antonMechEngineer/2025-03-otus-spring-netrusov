8c6ce6db8a5ddd843d916a94b8147424f1a42744
anetr <a.netrusov@mail.ru>
2025-10-01 09:30:13 +0300

    feat: added handling kafka errors

--- diplom/bank/src/main/java/ru/otus/hw/kafka/PaymentProducer.java
+++ diplom/bank/src/main/java/ru/otus/hw/kafka/PaymentProducer.java
@@ -1,15 +1,19 @@
 package ru.otus.hw.kafka;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.kafka.dto.PaymentResp;
 import ru.otus.hw.mappers.PaymentMapper;
 import ru.otus.hw.models.Payment;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PaymentProducer {
+
+    private static final String ERROR_SENDING = "Sending error {}, message: {}";
 
     private static final String TOPIC_RESP = "payment-response";
 
@@ -18,6 +22,10 @@ public class PaymentProducer {
     private final PaymentMapper paymentMapper;
 
     public void send(Payment payment) {
-        kafkaTemplate.send(TOPIC_RESP, paymentMapper.toPaymentResp(payment));
+        kafkaTemplate.send(TOPIC_RESP, paymentMapper.toPaymentResp(payment))
+                .exceptionally(t -> {
+                    log.error(ERROR_SENDING, t, payment);
+                    return null;
+                });
     }
 }
--- diplom/hotel/src/main/java/ru/otus/hw/kafka/PaymentProducer.java
+++ diplom/hotel/src/main/java/ru/otus/hw/kafka/PaymentProducer.java
@@ -1,14 +1,18 @@
 package ru.otus.hw.kafka;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.kafka.dto.PaymentReq;
 import ru.otus.hw.models.Order;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class PaymentProducer {
+
+    private static final String ERROR_SENDING = "Sending error {}, message: {}";
 
     private static final String TOPIC_REQ = "payment-request";
 
@@ -17,8 +21,11 @@ public class PaymentProducer {
     private final KafkaTemplate<String, PaymentReq> kafkaTemplate;
 
     public void send(Order o, PaymentReq.ActionType actionType) {
-        kafkaTemplate.send(
-                TOPIC_REQ,
-                new PaymentReq(BUY, o.getId(), o.getTotalPrice().doubleValue(), o.getUser().getUsername(), actionType));
+        var rq = new PaymentReq(BUY, o.getId(), o.getTotalPrice().doubleValue(), o.getUser().getUsername(), actionType);
+        kafkaTemplate.send(TOPIC_REQ, rq)
+                .exceptionally(t -> {
+                    log.error(ERROR_SENDING, t, rq);
+                    return null;
+                });
     }
 }
