db329efff621add643966411d55bdb42d217f2a6
anetr <a.netrusov@mail.ru>
2025-10-07 12:16:19 +0300

    chore: moved Dockerfiles and renamed main classes

--- diplom/bank/src/main/java/ru/otus/hw/BankApplication.java
+++ diplom/bank/src/main/java/ru/otus/hw/BankApplication.java
@@ -2,12 +2,10 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-public class Hotel {
+public class BankApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Hotel.class, args);
+		SpringApplication.run(BankApplication.class, args);
 	}
 }
--- diplom/hotel/src/main/java/ru/otus/hw/HotelApplication.java
+++ diplom/hotel/src/main/java/ru/otus/hw/HotelApplication.java
@@ -2,10 +2,12 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class Bank {
+@EnableScheduling
+public class HotelApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(Bank.class, args);
+		SpringApplication.run(HotelApplication.class, args);
 	}
 }
